2702c13645d798390bbeeac63069c612827e16cf
karl <karl3wm@writeme.com>
2025-03-23 07:11:21 -0400

    [trim commenting]

--- ndbigint2.py
+++ ndbigint2.py
@@ -137,76 +137,9 @@ class NDBigInt:
 
         x._data[...,:limbs] += y._data[...,:limbs]
         # in cases of overflow, the sum is less than the addend
-        # if the end limb overflows then another is needed
 
-        # the test for overflow differs in the last limb, which must be treated as a signed rather than unsigned number.
-        # this is because two large positive numbers can overflow into the sign bit, and this is still an overflow, but makes a greater unsigned value
-        # one approach would be to use a mask.
-        # notably the sign bit is always the highest bit of the last limb.
-        # the result is even correct: it is simply interpreted as a negative number.
-        # if it were treated as an overflow the code would continue correctly.
-        # there is likely a masked xor operation that would make the current code work.
-
-        # the overflow tests looks for sums that are less than addends.
-        # but certain large positive numbers overflow into the sign bit.
-        # this only happens when both initial numbers are positive (as otherwise the magnitude decreases), and the final number is negative.
-        # this sign bit is also set when the lost initial addend is negative and of greater magnitude than y, in which case the sum is less negative.
-
-        # a resource is that both numbers have been sign extended to an entire empty limb.
-        # this likely was expected to address situations like this.
-        # so we would have the original sign bit preserved if the summation didn't include that final empty limb.
-
-        # maybe i'll take a step here to address the sign of the result
-        # there are two possible overflows: a negative number can overflow into the last limb, and a positive number can overflow into the sign bit.
-        # i think these both only happen when the original sign bits are identical
-        # for negative overflow, two set sign bits become a value of lower total magnitude
-        #   no overflow
-        #         1  11  1
-        #           [11] 11
-        #         + [11] 11
-        #        -----------
-        #            11  10
-        #   overflow
-        #          1 11
-        #           [11] 11
-        #         + [11] 10
-        #        -----------
-        #            11  01
-        #   overflow by 2
-        #         1  11
-        #           [11] 10
-        #         + [11] 10
-        #        -----------
-        #            11  00
-        #   the sign bit overflows into the additional limb.
-        # for positive overflow, two unset sign bits become set
-        #   overflow
-        #           [00] 01
-        #         + [00] 01
-        #        -----------
-        #           [00] 10
-        #   now, like with negative, the additional limb is needed to preserve the sign.
-
-        #   in both conditions of overflow, the additional limb's original sign is what is valuable.
-        #   it could be kept as-is with sign extension
-
-        #   meanwhile, when a negative and a positive number are summed, the resulting sign relates to the addend of larger magnitude
-        #   it could be re-sign-extended from the portions summed.
-
-        # so, possibly two conditions: overflow, and non-overflow.
-        # for overflow, we want our original sign.
-        # for non-overflow, we want to sign extend our new sign bit.
-        # overflow can be detected by (a) the addend sign bits matching and (b) the sum sign bit mismatching
-        # this could be simplified into: if the addend sign bits match, then use the original limb
-        # if the addend sign bits mismatch, then sign-extend
-
-        #if xp.any(x._data[...,limbs-1] < y._data[...,limbs-1]):
-        #    limbs = alloc
-        #elif xp.any((x._data[...,limbs] == 0) & (y._data[...,limbs] == 0) & (x._data[...,limbs-1] > 0x8000000000000000)):
-        #    # a positive summation overflowed into the sign bit
-        #    limbs = alloc
         # if a limb is all 0xf, as for negative numbers, there will be multiple chained overflows
-        # one way to reduce the iterations here would be to amortize over many operations by maintaining overflow data, looping until it is 0 when __int__ is called
+        # one way to reduce the iterations here could be to amortize over many operations by maintaining overflow data, looping until it is 0 when __int__ is called
         ref = y._data[...,:limbs]
         off = 0
         while True:
@@ -217,80 +150,10 @@ class NDBigInt:
             off += 1
             x._data[...,off:limbs] += ref
 
-        # we want to sign extend only the numbers that have mismatching addend sign bits
-        # that is, the numbers where the new sign mismatches that in the additional limb
+        # sign extend
 
-        # how to calculate that. 3 parts .. y's top 65 bits, x's top 64 bits, and x's 65th bit the new sign bit
-        # - if bit 65 == bits 0-64, leave them as is
-        # - if bit 65 != bits 0-64, sign extend. if so, isn't that the same as inverting?
-        #   oh the new bit differs from the old. 3 values.
-        # - if y's bit -65 == x bits -64:, leave them as is
-        # - if y's bit -65 != x bits -64:, then sign extend x's bit -65 into bits -64:
-        # the problem is simplifiable into roughly conditioning the sign extend.
-        # - we can calculate the sign extension content by shifting bit -65
-        # - then we place it only if the sign mismatches
-        # we'd likely do the conditioning prior to the extending, might not matter
-        # so, what bit will we fill the end with?
-        # find boolean expression for this bit.
-        # - ybits[-65] == xbits[-1] -> xbits[-1] (or equivalently ybits[-65])
-        # - ybits[-65] != xbits[-1] -> xbits[-65]
-        # ysign x0sign  choice
-        # 0     0       0
-        # 0     1       x1sign
-        # 1     0       x1sign
-        # 1     1       1
-
-        # hrm
-        # ((ysign ^ x0sign) & x1sign) | ((~ysign ^ x0sign) & ysign)
-        # ~(~((ysign ^ x0sign) & x1sign) & ~(~(ysign ^ x0sign) & ysign))
-
-        # ysign x0sign  x1sign choice
-        # 0     0       0      0
-        # 0     0       1      0
-        # 0     1       0      0
-        # 0     1       1      1
-        # 1     0       0      0
-        # 1     0       1      1
-        # 1     1       0      1
-        # 1     1       1      1
-
-        # expanded table makes clearer that choice is ysign unless x0sign == x1sign != ysign
-        # so something like ysign ^ (x0sign ^ x1sign) with some nots?
-
-        # ysign x0sign  x1sign choice x0^x1
-        # 0     0       0      0      0
-        # 0     0       1      0      1
-        # 0     1       0      0      1
-        # 0     1       1      1      0
-        # 1     0       0      0      0
-        # 1     0       1      1      1
-        # 1     1       0      1      1
-        # 1     1       1      1      0
-
-        # oops it's ysign unless x0sign != ysign and x0sign == x1sign
-
-        # ysign x0sign  x1sign choice x0^x1 y^x0 y^x1  (y^x0)&(y^x1) y^((y^x0)&(y^x1))
-        # 0     0       0      0      0     0    0     0             0
-        # 0     0       1      0      1     0    1     0             0
-        # 0     1       0      0      1     1    0     0             0
-        # 0     1       1      1      0     1    1     1             1
-        # 1     0       0      0      0     1    1     1             0
-        # 1     0       1      1      1     1    0     0             1
-        # 1     1       0      1      1     0    1     0             1
-        # 1     1       1      1      0     0    0     0             1
-
-        # well that's 4 operations on a 1 bit value
-        # involving 2 temporaries that could be put into 1 word
-        # so the extra limb is initially filled with x0.
-        # could maybe place x1 into it somehow .. hrm
-        # if i could make part of it be x1 .. then ...
-        # this seems like too many operations. there must be a better approach. but i can try this one.
-
-        # do any existing things include this?
-        # - the carry code xors x1 with x0 when there is overflow if let to write to the extra limb
-        # if we continued the concept of 'overflow', one could identify overflow by comparing the sign changes in sequence
-        # x0sign == ysign && x1sign != x0sign
-        # so the logical == operator is useful because it's ~^. so there are more operations available if working with bool.
+        # there's likely a way to simplify this.
+        # "why is this needed? what case is addition with sign extension not covering?"
 
         # ysign x0sign  x1sign choice x0^x1 y^x0 y^x1  y^x0^x1  x0==x1  y==x0  y==x1   ((x0==x1)&(y^x0))^y
         # 0     0       0      0      0     0    0     0        1       1      1
@@ -302,21 +165,6 @@ class NDBigInt:
         # 1     1       0      1      1     0    1     0        0       1      0
         # 1     1       1      1      0     0    0     1        1       1      1
 
-        ## so we could make a bool tensor from x0, then compare it with x1, maybe ...
-        #x0_sign = xp.astype(x._data[...,limbs], dtype=xp.bool, copy=False)
-        #y_sign = xp.astype(y._data[...,limbs], dtype=xp.bool, copy=False)
-        #result_sign = y_sign ^ x0_sign
-        #x1_sign = x._data[...,limbs-1] >= 0x8000000000000000
-        #result_sign &= (x0_sign == x1_sign)
-        #result_sign ^= y_sign
-        ## might be convenient to work in full blocks; only x1 is unavailable in a full block. but it could be sign extended
-        ## but one could instead subtract a bool from 0 to turn it into a full block
-        ## it could be convenient if the result sign were in terms of x0 sign which is already the full block
-
-        # x0sign ^ ((x0^x1)&(y==x1))
-
-        # might not help too much
-
         # note i was earlier using signed >> 63 to convert 1 bit to 64 bits
         x0_sign = xp.astype(x._data[...,limbs], xp.bool, copy=False)
         y_sign = xp.astype(y._data[...,limbs], xp.bool, copy=False)
@@ -326,9 +174,8 @@ class NDBigInt:
         result_sign ^= y_sign
         x._data[...,limbs:] = xp.astype(-xp.astype(result_sign[...,None], xp.int64, copy=False), xp.uint64, copy=False)
 
-        # sign extend, set limbs
+        # set limbs
         # probably efficiency improvements exist
-
         if xp.any((x._data[...,limbs]) ^ (x._data[...,limbs-1]>>63)):
             limbs += 1
         else:
