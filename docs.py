f7fdb6d3fe77ef0d3e8aef94be849ab7436b1ed3
karl <karl3wm@writeme.com>
2025-03-27 10:18:07 -0400

    doc resource that can be used during debugging etc

---
+++ docs.py
@@ -0,0 +1,21 @@
+import _xp_docs as docs
+import sys
+
+def inject_docs(xp):
+    for fn, docf in docs.__dict__.items():
+        try:
+            getattr(xp, fn).__doc__ = docf.__doc__
+        except AttributeError:
+            pass
+
+for xp_modname in [
+    'array_api_strict',
+    'array_api_compat.common',
+    'array_api_compat.cupy',
+    'array_api_compat.numpy',
+    'array_api_compat.torch',
+]:
+    if xp_modname in sys.modules:
+        inject_docs(sys.modules[xp_modname])
+if 'numpy' in sys.modules:
+    inject_docs(sys.modules['numpy'].generic)
